e2f1131b88dcdfe0c1f3375e8503ac7d757273b5
Reda BEGGAR <reda.beggar@corp.talan.com>
2018-01-22 10:28:50 +0100

    add operation request mapping: MAKE A DEPOSIT to -operation controller-

--- src/main/java/com/redabeggar/bankAccountApi/controller/OperationController.java
+++ src/main/java/com/redabeggar/bankAccountApi/controller/OperationController.java
@@ -1,6 +1,13 @@
 package com.redabeggar.bankAccountApi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.redabeggar.bankAccountApi.model.Operation;
+import com.redabeggar.bankAccountApi.service.OperationService;
+import com.redabeggar.bankAccountApi.utils.OperationRequest;
 
 
 @RestController
@@ -8,4 +15,14 @@ public class OperationController {
 
 	
 	
+	@Autowired
+	private OperationService operationService;
+
+
+	@PostMapping(path = "/deposit", consumes = "application/json", produces = "application/json")
+	public Operation createDeposit(@RequestBody OperationRequest operationRequest) {
+
+		return operationService.makeADeposit(operationRequest);
+
+	}
 }
